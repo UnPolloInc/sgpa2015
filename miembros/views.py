17d2e24e9f6dc6ee3e16de9e822c92f9763cbeb1
alforro <alfa.alvaro.rodriguez@gmail.com>
2015-05-29 14:59:59 -0400

    agregamos lider como variable de contexto en listar miembros

--- miembros/views.py
+++ miembros/views.py
@@ -101,6 +101,7 @@ class IndexView(ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['proyecto'] = Proyecto.objects.get(pk=self.kwargs['pk'])
+        context['lider'] = Usuario.objects.get(pk=self.request.user)
         return context
 
     def get_queryset(self):
