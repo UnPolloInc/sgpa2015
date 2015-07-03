ed45ea10e0530867b76cd069b723df59b7e75dd1
alforro <alfa.alvaro.rodriguez@gmail.com>
2015-07-03 19:47:03 -0400

    lista de proyectos para superuser

--- proyectos/views.py
+++ proyectos/views.py
@@ -74,7 +74,10 @@ class IndexView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['lider'] = Usuario.objects.get(pk=self.request.user)
+        try:
+            context['lider'] = Usuario.objects.get(pk=self.request.user)
+        except:
+            context['lider'] = None
         return context
 
 class ProyectoMixin(object):
